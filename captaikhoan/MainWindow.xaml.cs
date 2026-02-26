f5e03a5fd1a80317815c00cc1753461600f756e3
Nguyen Duc Thien <thien12514@gmail.comxad>
2026-02-26 19:03:37 +0700

    Add project files.

---
+++ captaikhoan/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CapTaiKhoan
+{
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<User> users = new ObservableCollection<User>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgUsers.ItemsSource = users;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUser.Text))
+            {
+                MessageBox.Show("Nhập user");
+                return;
+            }
+
+            string role = ((ComboBoxItem)cbRole.SelectedItem).Content.ToString();
+
+            users.Add(new User
+            {
+                Username = txtUser.Text,
+                Password = txtPass.Password,
+                Role = role
+            });
+
+            txtUser.Clear();
+            txtPass.Clear();
+        }
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
