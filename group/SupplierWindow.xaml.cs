8dba7cdebd734fd6678d39357978c392bad30e86
trung <do21082005@gmail.com>
2026-02-26 12:51:12 +0700

    update project

---
+++ group/SupplierWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace group
+{
+    public partial class SupplierWindow : Window
+    {
+        public SupplierWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnThem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Thêm");
+        }
+
+        private void BtnSua_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sửa");
+        }
+
+        private void BtnXoa_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Xóa");
+        }
+
+        private void BtnTimKiem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Tìm kiếm");
+        }
+    }
+}
