adb1562d9319ddbe03d277bb245322ad1e25490e
Fundamentals-of-Computing <ngovannhat221205@gmail.com>
2026-03-07 11:58:27 +0700

    add project QuanlyTonKho_NEW

---
+++ QuanlyTonkho_NEW - Sao chép/QuanlyTonkho_NEW/Helpers/DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace QuanlyTonkho_NEW.Helpers
+{
+    public class DBConnection
+    {
+        public static MySqlConnection GetConnection()
+        {
+            string connStr =
+            "server=localhost;" +
+            "database=phieuxuatnew;" +
+            "uid=appuser;" +
+            "pwd=123456;" +
+            "SslMode=none;" +
+            "AllowPublicKeyRetrieval=true;";
+
+            return new MySqlConnection(connStr);
+        }
+    }
+}
